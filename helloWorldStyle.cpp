d80e4bde42b0e00056620d49bdb016ff031d5497
Noah-234 <noah.ouellette@stu.ocsb.ca>
2021-11-24 06:09:41 +0000

    Created and finished Hello World program with style using C++

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2021 Noah Ouellette All rights reserved.
+
+//  .
+
+// Created by: Noah Ouellette
+
+// Date: Nov. 23, 2021
+
+// This program displays "Hello World!" using proper cpp style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World! Now with some serious style!" << std::endl;
+}
